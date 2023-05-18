6acd1df3822afc875f3768806cbf26565bb8dfd6
PUJARI RAKESH <rpujari@gitam.in>
2023-05-18 12:47:49 +0530

    3rd commit

--- wp-admin/upload.php
+++ wp-admin/upload.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=https://rakesh737-hub.github.io/cognis/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D12612&reauth=1">
+		<meta http-equiv="refresh" content="0;url=https://rakesh737-hub.github.io/cognis/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D13734&reauth=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "https://rakesh737-hub.github.io/cognis/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D12612&reauth=1";
+			window.location = "https://rakesh737-hub.github.io/cognis/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D13734&reauth=1";
 		</script>
 
-		<p>You are being redirected to <a href="https://rakesh737-hub.github.io/cognis/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D12612&reauth=1">https://rakesh737-hub.github.io/cognis/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D12612&reauth=1</a></p>
+		<p>You are being redirected to <a href="https://rakesh737-hub.github.io/cognis/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D13734&reauth=1">https://rakesh737-hub.github.io/cognis/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D13734&reauth=1</a></p>
 	</body>
 </html>
